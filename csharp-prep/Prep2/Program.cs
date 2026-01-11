a5e13745f9bf3028dfea9ee7ee5a56aa87975dac
ShareYourHead <tjnewbry@gmail.com>
2026-01-10 20:25:22 -0700

    W01 Prove: Developer - C# Programming Parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeStr = Console.ReadLine();
+        float grade = float.Parse(gradeStr);
+
+
+
+        string letter = string.Empty;
+
+        if (grade >= 90.0)
+            letter = "A";
+        else if (grade >= 80.0)
+            letter = "B";
+        else if (grade >= 70.0)
+            letter = "C";
+        else if (grade >= 60.0)
+            letter = "D";
+        else
+            letter = "F";
+
+        Console.WriteLine($"Your grade: {letter}");
+
+
+
+        if(grade >= 70.0)
+            Console.WriteLine("Congratulations on the passing grade!");
+        else
+            Console.WriteLine("DO CREDIT FOR YOU! Start fresh next semester.");
+
     }
 }
